05c6ffe405e359bbdf20a6edbb22ebb922b0a254
James Stewart <jwds1978@users.noreply.github.com>
2017-02-06 14:11:29 -0500

    Create mask-phone-numbers.js

---
+++ scripts/mask-phone-numbers.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+//<![CDATA[
+  $(function() {
+    $('#1887').toggle(function() {
+      $(this).find('span').html('&times;&times;&times;&times;');
+    }, function() {
+      $(this).find('span').text($(this).data('last'));
+    })
+    .click();
+  });
+  $(function() {
+    $('#3664').toggle(function() {
+      $(this).find('span').html('&times;&times;&times;&times;');
+    }, function() {
+      $(this).find('span').text($(this).data('last'));
+    })
+    .click();
+  });
+//]]> 
+</script>
