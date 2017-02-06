c90e391512b90c063436dcc311981307ef337c2d
James Stewart <jwds1978@users.noreply.github.com>
2017-02-06 14:20:06 -0500

    Update mask-phone-numbers.js

--- scripts/mask-phone-numbers.js
+++ scripts/mask-phone-numbers.js
@@ -1,18 +1,17 @@
-<script type="text/javascript">
-  $(function() {
-    $('#1887').toggle(function() {
-      $(this).find('span').html('&times;&times;&times;&times;');
-    }, function() {
-      $(this).find('span').text($(this).data('last'));
-    })
-    .click();
-  });
-  $(function() {
-    $('#3664').toggle(function() {
-      $(this).find('span').html('&times;&times;&times;&times;');
-    }, function() {
-      $(this).find('span').text($(this).data('last'));
-    })
-    .click();
-  });
-</script>
+$(function() {
+  $('#1887').toggle(function() {
+    $(this).find('span').html('&times;&times;&times;&times;');
+  }, function() {
+    $(this).find('span').text($(this).data('last'));
+  })
+  .click();
+});
+
+$(function() {
+  $('#3664').toggle(function() {
+    $(this).find('span').html('&times;&times;&times;&times;');
+  }, function() {
+    $(this).find('span').text($(this).data('last'));
+  })
+  .click();
+});
