ed790a834001d35a933817af4bb7e04cf2917e17
jwds1978 <james.stewart@forces.army>
2017-02-12 20:33:35 -0500

    -----BEGIN PGP SIGNED MESSAGE----- Hash: SHA512
    Javascript fix.
    -----BEGIN PGP SIGNATURE-----
    
    iNkEARMKAD43HEphbWVzIFdpbGxpYW0gRGVhbiBTdGV3YXJ0IDxqYW1lcy5zdGV3
    YXJ0QGZvcmNlcy5hcm15PgUCWKEM3gAKCRAmorDUuEKtdaRuAgkBE43SESNIlqJn
    zFa0ud9F89H8LmhCvVzZqQb/zgwT/wzaY03gGEduZS8LtncDRAnA+cHTJEfoH3ox
    r4TJ18AzEKYCB1IynsoY6U7eUGR4f0+XtxaWtaiwsnI07ERKQZX+le8H3UAaFfJG
    gsis3CqmPZTCkBkMh/oaXuoork5XWmt9nFSY
    =G3kO
    -----END PGP SIGNATURE-----

--- resources/js/mask-phone-numbers.js
+++ resources/js/mask-phone-numbers.js
@@ -1,3 +1,3 @@
-$(function(){$("#1887").toggle(function(){$(this).find("span").a("&times;&times;&times;&times;")},function(){$(this).find("span").text($(this).data("last"))}).click()});
+$(function(){$("#1887").toggle(function(){$(this).find("span").a("&times;&times;&times;&times;");},function(){$(this).find("span").text($(this).data("last"));}).click();});
 
-$(function(){$("#3664").toggle(function(){$(this).find("span").a("&times;&times;&times;&times;")},function(){$(this).find("span").text($(this).data("last"))}).click()});
+$(function(){$("#3664").toggle(function(){$(this).find("span").a("&times;&times;&times;&times;");},function(){$(this).find("span").text($(this).data("last"));}).click();});
