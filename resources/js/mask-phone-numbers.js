9a2629dba5df30c9575af273803d98bb774fdc02
James Stewart <jwds1978@users.noreply.github.com>
2017-02-17 15:21:12 -0500

    Update mask-phone-numbers.js

--- resources/js/mask-phone-numbers.js
+++ resources/js/mask-phone-numbers.js
@@ -4,7 +4,6 @@ $(function() {
   }, function() {
     $(this).find("span").text($(this).data("last"));
   }).click();
-
   $("#3664").toggle(function() {
     $(this).find("span").html("&times;&times;&times;&times;");
   }, function() {
