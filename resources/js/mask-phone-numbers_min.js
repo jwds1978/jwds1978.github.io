b49b3fbf49297d9995d0021b875a5850a7e462df
James Stewart <jwds1978@users.noreply.github.com>
2017-02-17 16:07:02 -0500

    Create mask-phone-numbers_min.js

---
+++ resources/js/mask-phone-numbers_min.js
@@ -0,0 +1,11 @@
+$(function () {
+  $("#1887").toggle(function () {
+    $(this).find("span").html("&times;&times;&times;&times;")
+  }, function () {
+    $(this).find("span").text($(this).data("last"))
+  }).click(), $("#3664").toggle(function () {
+    $(this).find("span").html("&times;&times;&times;&times;")
+  }, function () {
+    $(this).find("span").text($(this).data("last"))
+  }).click()
+});
