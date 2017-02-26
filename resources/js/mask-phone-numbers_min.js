3bb473683e07329b12368c4e60292f587ea6305f
James Stewart <jwds1978@users.noreply.github.com>
2017-02-26 15:10:48 -0500

    Update mask-phone-numbers_min.js

--- resources/js/mask-phone-numbers_min.js
+++ resources/js/mask-phone-numbers_min.js
@@ -3,6 +3,10 @@ $(function () {
     $(this).find("span").html("&times;&times;&times;&times;")
   }, function () {
     $(this).find("span").text($(this).data("last"))
+  }).click(), $("#3403").toggle(function () {
+    $(this).find("span").html("&times;&times;&times;&times;")
+  }, function () {
+    $(this).find("span").text($(this).data("last"))
   }).click(), $("#3664").toggle(function () {
     $(this).find("span").html("&times;&times;&times;&times;")
   }, function () {
