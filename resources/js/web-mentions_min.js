87eafed311afc2213b4e2f38d28e548409512aef
James Stewart <james.stewart@forces.army>
2017-03-03 15:21:16 -0500

    Web mentions.

---
+++ resources/js/web-mentions_min.js
@@ -0,0 +1,19 @@
+---
+sitemap: false
+---
+
+{{ site.copyright.comment.java }}
+jQuery(function (a) {
+  var c, b = [];
+  a("*[data-webmention-count]").each(function (d, e) {
+    var f = document.createElement("a");
+    f.href = a(e).data("url"), c = f.protocol + "//" + f.hostname, b.push(f.pathname + f.search)
+  }), a.getJSON("https://webmention.io/api/count?jsonp=?", {
+    base: c,
+    targets: b.join(",")
+  }, function (b) {
+    a("*[data-webmention-count]").each(function (c, d) {
+      a(d).text(b.count[a(d).data("url")])
+    })
+  })
+});
