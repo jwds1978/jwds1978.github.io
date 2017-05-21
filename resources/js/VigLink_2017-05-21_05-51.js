1d15a52047ddd330a0bfdc1fd2973a4dbfa48647
James Stewart <james.stewart@forces.army>
2017-05-21 05:57:15 -0400

    VigLink code.

---
+++ resources/js/VigLink_2017-05-21_05-51.js
@@ -0,0 +1,18 @@
+---
+sitemap: false
+---
+
+{{ site.copyright.comment.java }}
+
+var vglnk = {
+  key: 'f1a1eb9547ce54ae2b0724abe201765c'
+};
+
+(function (d, t) {
+  var s = d.createElement(t);
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = 'https://cdn.viglink.com/api/vglnk.js';
+  var r = d.getElementsByTagName(t)[0];
+  r.parentNode.insertBefore(s, r);
+}(document, 'script'));
---
+++ resources/js/VigLink_2017-05-21_05-51_min.js
@@ -0,0 +1,14 @@
+---
+sitemap: false
+---
+
+{{ site.copyright.comment.java }}
+var vglnk = {
+  key: "f1a1eb9547ce54ae2b0724abe201765c"
+};
+! function (a, b) {
+  var c = a.createElement(b);
+  c.type = "text/javascript", c.async = !0, c.src = "https://cdn.viglink.com/api/vglnk.js";
+  var d = a.getElementsByTagName(b)[0];
+  d.parentNode.insertBefore(c, d)
+}(document, "script");
