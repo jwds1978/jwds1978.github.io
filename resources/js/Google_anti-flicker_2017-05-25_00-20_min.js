833dc931ade17747a17ddb83ae1c40cbfd31b22d
James Stewart <james.stewart@forces.army>
2017-05-25 00:23:47 -0400

    Google Optimize.

---
+++ resources/js/Google_anti-flicker_2017-05-25_00-20_min.js
@@ -0,0 +1,14 @@
+---
+sitemap: false
+---
+
+{{ site.copyright.comment.java }}
+! function (a, b, c, d, e, f, g, h, i) {
+  b.className += " " + c, f.start = 1 * new Date, f.end = g = function () {
+    b.className = b.className.replace(RegExp(" ?" + c), "")
+  }, (a[d] = a[d] || []).hide = f, setTimeout(function () {
+    g(), f.end = null
+  }, e), f.timeout = e
+}(window, document.documentElement, "async-hide", "dataLayer", 4e3, {
+  "GTM-M84X859": !0
+});
